482608a5901f25b2ae076f8ab73b0befa795be24
Daniel Decsi <daniel.decsi@supercharge.io>
2020-03-22 11:49:48 +0100

    style: format files API files with Prettier

--- api/src/app/app.module.ts
+++ api/src/app/app.module.ts
@@ -1,6 +1,6 @@
-import { Module } from '@nestjs/common';
+import { Module } from '@nestjs/common'
 
-import { AppController } from './controllers/app.controller';
+import { AppController } from './controllers/app.controller'
 
 @Module({
   imports: [],
--- api/src/app/controllers/app.controller.ts
+++ api/src/app/controllers/app.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get } from '@nestjs/common'
 
 @Controller()
 export class AppController {
   @Get()
   public getHealth(): string {
-    return '👑';
+    return '👑'
   }
 }
